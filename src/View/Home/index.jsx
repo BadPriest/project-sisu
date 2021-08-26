dc1d8839ee9768726533ee0820497fb03dfb33b9
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-08-26 11:37:52 -0300

    feat(home): adds stubbed <Home /> component

---
+++ src/View/Home/index.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+import { StyledHomeWrapper } from "./styles";
+
+export const Home = () => (
+  <StyledHomeWrapper>
+    <></>
+  </StyledHomeWrapper>
+);
+
+export default Home;
