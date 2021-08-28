7a82bc120f8511312a9b8b98c52e85215484e426
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-08-28 15:07:10 -0300

    feat(theme colors): adds color props on theme

--- src/Core/Theme/index.js
+++ src/Core/Theme/index.js
@@ -1,9 +1,16 @@
 export const Theme = {
+  colors: {
+    primary: "#3d5a80",
+    secondary: "#98c1d9",
+    success: "#4ecdc4",
+    danger: "#ee6c4d",
+    warning: "#fcca46",
+  },
   typography: {
     bodyCopyColor: "#403d39",
     headings: {
       fontFamily: "montserratextrabold",
-      h1: { fontSize: "32px" },
+      h1: { fontSize: "32px", fontWeight: "800" },
     },
     body: {
       fontFamily: "montserratthin",
