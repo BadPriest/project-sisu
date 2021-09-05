1bd03b86a544f106b1759412df09abf0661e91d1
bruno v vergatti <bruno.vergatti@imaginebeyond.com.br>
2021-09-05 19:45:40 -0300

    feat(usesortcollection): adds useSortCollection, a hook for sorting any collection with given prop
    Hook with prop-based bi-directional sorting. It can sort a given collection through a given number,
    string or date prop.

---
+++ src/View/Shared/Hooks/useSortCollection.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import moment from "moment";
+
+import { isValidDate } from "../../../Utils/parseDates";
+
+export const SORTING_ORDER = {
+  ASCENDING: "ascending",
+  DESCENDING: "descending",
+};
+
+export const getNewSortableConfig = (key, direction) => ({
+  key,
+  direction,
+});
+
+export const useSortCollection = (collection, config = null) => {
+  const [sortConfig, setSortConfig] = React.useState(config);
+
+  const compareNumbers = (a, b, direction) =>
+    direction === SORTING_ORDER.ASCENDING ? +a - +b : +b - +a;
+
+  const compareStrings = (a, b, direction) =>
+    direction === SORTING_ORDER.ASCENDING
+      ? a.localeCompare(b, "pt-BR")
+      : b.localeCompare(a, "pt-BR");
+
+  const compareDates = (a, b, direction) => {
+    const dateA = moment(a);
+    const dateB = moment(b);
+
+    if (direction === SORTING_ORDER.ASCENDING) {
+      return dateA.isBefore(dateB) ? -1 : 1;
+    }
+
+    if (direction === SORTING_ORDER.DESCENDING) {
+      return dateB.isBefore(dateA) ? -1 : 1;
+    }
+
+    return 0;
+  };
+
+  const getComparisonFunction = (value) => {
+    if (typeof value === "number") {
+      return compareNumbers;
+    }
+
+    if (typeof value === "string") {
+      return isValidDate(value) ? compareDates : compareStrings;
+    }
+
+    return [...collection].sort;
+  };
+
+  const sortedCollection = React.useMemo(() => {
+    const sorted = [...collection];
+    if (sortConfig !== null && sorted) {
+      const { key } = sortConfig;
+
+      sorted.sort((a, b) => {
+        const propValue = a[key];
+        const compare = getComparisonFunction(propValue);
+
+        return compare(
+          a[sortConfig.key],
+          b[sortConfig.key],
+          sortConfig.direction
+        );
+      });
+    }
+
+    return sorted;
+  }, [collection, sortConfig]);
+
+  const getDirection = (key) => {
+    let direction = SORTING_ORDER.ASCENDING;
+    const keyWasSorted = sortConfig && sortConfig.key === key;
+    const isAscending =
+      sortConfig && sortConfig.direction === SORTING_ORDER.ASCENDING;
+
+    if (keyWasSorted && isAscending) {
+      direction = SORTING_ORDER.DESCENDING;
+    }
+
+    return direction;
+  };
+
+  const requestSort = (key) => {
+    if (!collection || collection.length === 0) {
+      return;
+    }
+
+    const direction = getDirection(key);
+    setSortConfig({ key, direction });
+  };
+
+  return { sortedCollection, requestSort, sortConfig };
+};
+
+export default useSortCollection;
