5140d7e885b39602c05909e2fea4e3381d71134a
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-09-07 17:03:49 -0300

    feat(projectsavings): replaces search logic to use SearchProjects instead

--- src/View/ProjectSavings/index.jsx
+++ src/View/ProjectSavings/index.jsx
@@ -3,37 +3,36 @@ import * as React from "react";
 import { SectionTitle } from "../Shared/SectionTitle";
 import { Separator } from "../Shared/Separator";
 
-import { useSearchCollection } from "../Shared/Hooks/useSearchCollection";
-
 import { useProjectSavingsData } from "./useProjectSavingsData";
-import { filterCollection as filterFlattenCollection } from "../Shared/Utils/filterFlattenCollection";
 
-import { Input as SearchInput } from "../Shared/Input";
-
+import { SearchProjects } from "./SearchProjects";
 import { SortProjects } from "./SortProjects";
 import { ListProjects } from "./ListProjects";
 
 import { StyledWrapper, SectionHeader, StyledContentWrapper } from "./styles";
 
 export const ProjectSavings = () => {
-  const [processedProjects, setProcessedProjects] = React.useState();
-
   const { projects } = useProjectSavingsData();
-  const { setQuery, filteredCollection } = useSearchCollection(
-    projects,
-    filterFlattenCollection
-  );
+
+  // ? there's got to be a better way ?
+  const [filteredProjects, setFilteredProjects] = React.useState();
+  const [processedProjects, setProcessedProjects] = React.useState();
 
   return (
     <StyledWrapper>
       <SectionHeader>
         <SectionTitle>Project Savings Data</SectionTitle>
-        <SearchInput type="text" name="inputSearch" onChange={setQuery} />
+        {projects && (
+          <SearchProjects
+            projects={projects}
+            updateSearch={setFilteredProjects}
+          />
+        )}
       </SectionHeader>
       <Separator height="2em" />
-      {filteredCollection && (
+      {filteredProjects && (
         <SortProjects
-          projects={filteredCollection}
+          projects={filteredProjects}
           updateSort={setProcessedProjects}
         />
       )}
