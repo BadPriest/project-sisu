ec1881b4f48b7a78a9f55983e61541e2ef4ff962
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-09-16 09:17:09 -0300

    refactor(card): removes Project Card specific styles + removes unused styles

--- src/View/Shared/Card.js
+++ src/View/Shared/Card.js
@@ -1,6 +1,6 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
-const BaseCard = styled.article`
+const ResetCard = styled.article`
   margin: 0;
   padding: 0;
   background-color: ${({ theme }) => theme.colors.surface};
@@ -8,60 +8,26 @@ const BaseCard = styled.article`
   line-height: normal;
 `;
 
-export const Card = styled(BaseCard)`
+export const BaseCard = styled(ResetCard)`
   position: relative;
   min-width: 330px;
   max-width: 330px;
-  min-height: 270px;
-  max-height: 270px;
 
-  padding: 2.5em 1em;
   border-radius: 0.3em;
 
   font-size: 0.9rem;
 
   overflow: hidden;
-  isolation: isolate;
   transition: 0.2s ease;
 
   box-shadow: 0 1px 1px 0 rgba(66, 66, 66, 0.08),
     0 1px 3px 1px rgba(66, 66, 66, 0.16);
 
   &:hover {
-    z-index: 1;
     transform: scale(1.1);
     box-shadow: 0px 10px 13px -7px #00000066,
       12px 10px 14px -8px rgba(103, 103, 103, 0);
   }
-
-  &:before {
-    content: " ";
-    display: inline-block;
-    position: absolute;
-    inset: 0;
-    z-index: -1;
-    height: 1.5em;
-
-    ${({ theme, complexity }) => css`
-      ${complexity === "simple" &&
-      css`
-        background-color: ${theme.colors.success};
-        color: ${theme.colors.text};
-      `}
-
-      ${complexity === "moderate" &&
-      css`
-        background-color: ${theme.colors.warning};
-        color: ${theme.colors.text};
-      `}
-
-      ${complexity === "hazardous" &&
-      css`
-        background-color: ${theme.colors.danger};
-        color: ${theme.colors.background};
-      `}
-    `}
-  }
 `;
 
-export default Card;
+export default BaseCard;
