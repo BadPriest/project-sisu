15d65291a9e2bd7acb6b6425fd813de386f692f4
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-09-16 08:57:32 -0300

    feat(theme props): adds fontWeights and headings, adjusts body.small.fontSize

--- src/Core/Theme/index.js
+++ src/Core/Theme/index.js
@@ -4,14 +4,23 @@ import { light } from "./color-presets";
 const colors = light;
 
 const typography = {
+  fontWeights: {
+    thin: 300,
+    regular: 400,
+    semibold: 500,
+    bold: 600,
+  },
   headings: {
     fontFamily: "bebas_neueregular",
     h1: { fontSize: "3rem", fontWeight: "800" },
+    h2: { fontSize: "2rem", fontWeight: "800" },
+    h3: { fontSize: "1.5rem", fontWeight: "800" },
+    h4: { fontSize: "1.2rem", fontWeight: "800" },
     display: { fontSize: "4.5rem", fontWeight: "800" },
   },
   body: {
     fontFamily: "barlowlight",
-    small: { fontSize: "0.7rem" },
+    small: { fontSize: "0.8rem" },
     regular: { fontSize: "1rem" },
     large: { fontSize: "1.1rem" },
   },
