5a4976a60480f44738a6c736cd80e2c5a1e43b4f
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-09-16 10:11:22 -0300

    refactor(card width): adjusts card width to fit all presentation options

--- src/View/ProjectSavings/ListProjects/styles.js
+++ src/View/ProjectSavings/ListProjects/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const StyledListWrapper = styled.section`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(270px, 330px));
+  grid-template-columns: repeat(auto-fill, minmax(270px, 470px));
   grid-column-gap: 1.5rem;
   grid-row-gap: 2rem;
 
