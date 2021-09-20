c0e29fc40ce750352dab494bb5143f46f5d45236
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-09-20 13:47:18 -0300

    refactor(input): completes base style, removes basic Input styled component

--- src/View/Shared/Input.js
+++ src/View/Shared/Input.js
@@ -1,32 +1,27 @@
 import styled from "styled-components";
-import { StyledControlsWrapper as ProjectSavingsControlsHeader } from "../ProjectSavings/styles";
 
 export const BaseInput = styled.input`
   display: block;
 
-  font-size: 14px;
-  line-height: 2.2rem;
-  font-family: ${(props) => props.theme.typography.headings.fontFamily};
+  line-height: 2.2em;
+  font-family: ${(props) => props.theme.typography.body.fontFamily};
+  font-size: ${(props) => props.theme.typography.headings.h3.fontSize};
+  padding: 0 1em;
 
   outline: none;
   border: none;
-  border-bottom: solid 0.15rem ${(props) => props.theme.colors.text}99;
+  border-bottom: solid 0.3rem ${(props) => props.theme.colors.text}99;
   border-radius: 0.1rem;
 
   color: ${({ theme }) => theme.colors.text};
   background-color: ${({ theme }) => theme.colors.surface};
-`;
+  transition: 0.2s ease-in;
 
-// ? Styles variations based on where it's being presented.
-// ? In theory, this can help us to mantain all the styles
-// ? for a given element within a single file/module,
-// ? instead of spreading through the application
-// ! What's the impact on code-bundling when all styles
-// ! are centralized like so?
-export const Input = styled(BaseInput)`
-  ${ProjectSavingsControlsHeader} & {
-    max-height: 4rem;
-    min-width: 50vw;
-    grid-area: search;
+  &:focus {
+    border-color: ${({ theme }) => theme.colors.primary};
+    transition: 0.2s ease-out;
+    outline: none;
   }
 `;
+
+export default BaseInput;
