73977d527696960499889dc5331f05d2c32e6b1b
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-10-06 19:07:26 -0300

    feat(feedbacks): adds feedback related to data state (loading, error, empty search results)

--- src/View/ProjectSavings/index.jsx
+++ src/View/ProjectSavings/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 import { SectionTitle } from "../Shared/SectionTitle";
 import { Separator } from "../Shared/Separator";
 import { useProjectSavingsData } from "./useProjectSavingsData";
-import { FeedbackDataEmpty } from "../Shared/Feedback/DataEmpty";
+import { FeedbackSearchEmpty } from "../Shared/Feedback/SearchEmpty";
+import { FeedbackDataEmptyResponse } from "../Shared/Feedback/DataEmpty";
+import { FeedbackDataError } from "../Shared/Feedback/DataError";
 
 import { SearchProjects } from "./SearchProjects";
 import { SortProjects } from "./SortProjects";
@@ -17,11 +19,32 @@ import {
 } from "./styles";
 
 export const ProjectSavings = () => {
-  const { projects } = useProjectSavingsData();
+  const { projects, error, loading } = useProjectSavingsData();
 
   // ? there's got to be a better way
   const [filteredProjects, setFilteredProjects] = React.useState();
   const [processedProjects, setProcessedProjects] = React.useState();
+
+  const hasError = !loading && error;
+  const hasProjectsAvailable = projects?.length && !hasError;
+  const noProjectsAvailable = !projects?.length && !hasError;
+  const searchNoResults = hasProjectsAvailable && !processedProjects?.length;
+
+  const renderContent = () => {
+    if (noProjectsAvailable) {
+      return <FeedbackDataEmptyResponse />;
+    }
+
+    if (hasError) {
+      return <FeedbackDataError />;
+    }
+
+    if (searchNoResults) {
+      return <FeedbackSearchEmpty />;
+    }
+
+    return <ListProjects projects={processedProjects} />;
+  };
 
   return (
     <StyledWrapper>
@@ -32,30 +55,19 @@ export const ProjectSavings = () => {
       <Separator height="2.5em" />
 
       <StyledControlsWrapper>
-        {projects && (
-          <SearchProjects
-            projects={projects}
-            updateSearch={setFilteredProjects}
-          />
-        )}
-
-        {filteredProjects && (
-          <SortProjects
-            projects={filteredProjects}
-            updateSort={setProcessedProjects}
-          />
-        )}
+        <SearchProjects
+          projects={projects}
+          updateSearch={setFilteredProjects}
+        />
+        <SortProjects
+          projects={filteredProjects}
+          updateSort={setProcessedProjects}
+        />
       </StyledControlsWrapper>
 
       <Separator height="2.5em" />
 
-      <StyledContentWrapper>
-        {processedProjects?.length ? (
-          <ListProjects projects={processedProjects} />
-        ) : (
-          <FeedbackDataEmpty />
-        )}
-      </StyledContentWrapper>
+      <StyledContentWrapper>{renderContent()} </StyledContentWrapper>
     </StyledWrapper>
   );
 };
