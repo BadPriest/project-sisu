d18c0f536cc15425f1d239b6d91a9d65379c4c05
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-10-10 18:12:49 -0300

    feat(testfixtures): adds 'test fixture' for a simple data collection

---
+++ src/Utils/TestFixtures/constants.js
@@ -0,0 +1,7 @@
+export const someCollectionFixture = [
+  { order: 1, amount: 1, name: "mercury", normalizedDate: "01.01.1990" },
+  { order: 2, amount: 2, name: "venus", normalizedDate: "01.01.2000" },
+  { order: 3, amount: 3, name: "earth", normalizedDate: "01.01.2010" },
+];
+
+export default someCollectionFixture;
