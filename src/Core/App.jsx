fd493a747ba85126162b3daf976a44e828cca3c4
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-10-10 15:02:36 -0300

    feat(app): sets FeedbackDataLoading as ProjectSavings/Suspense fallback

--- src/Core/App.jsx
+++ src/Core/App.jsx
@@ -7,13 +7,14 @@ import { ViewTemplate } from "./ViewTemplate";
 import { ProjectSavings } from "../View/ProjectSavings";
 
 import { StyledAppWrapper } from "./styles";
+import { FeedbackDataLoading } from "../View/Shared/Feedback/DataLoading";
 
 function App() {
   return (
     <StyledAppWrapper>
       <ThemeProvider theme={Theme}>
         <ViewTemplate>
-          <Suspense fallback={<>Loading...</>}>
+          <Suspense fallback={<FeedbackDataLoading/>}>
             <ProjectSavings />
           </Suspense>
         </ViewTemplate>
