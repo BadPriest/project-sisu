a21c9301f88d3c565dc768d0f3d6b03ff40959cc
Bruno V Vergatti <bruno_vergatti@yahoo.com.br>
2021-10-10 18:18:05 -0300

    fix(filterflattencollection): converts query param to string before using String methods

--- src/View/Shared/Utils/filterFlattenCollection.js
+++ src/View/Shared/Utils/filterFlattenCollection.js
@@ -1,9 +1,11 @@
 import { normalizeString } from "../../../Utils/parseStrings";
 
-const getFlatValues = (item) =>
+export const getFlatValues = (item) =>
   normalizeString(JSON.stringify(Object.values(item)));
 
 export const filterCollection = (collection, query) =>
-  collection.filter((i) => getFlatValues(i).includes(normalizeString(query)));
+  collection.filter((i) =>
+    getFlatValues(i).includes(normalizeString(String(query)))
+  );
 
 export default filterCollection;
